0610e5cbffc1a5b2cac5b0e38fb38a9c0d30dcfd
collinskoech11 <collinskoechck34@gmail.com>
2020-08-21 14:27:19 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+document.getElementById('open').addEventListener('click',
+    function(){
+        document.querySelector('.modal').style.display = 'block';
+    });
+
+document.getElementById('close').addEventListener('click',
+    function(){
+        document.querySelector('.modal').style.display = 'none';
+    });
